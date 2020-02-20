fb2e3c24ba345af7533d8ac5ff5e5eaac7974b31
blewgrassky <blewgrassdalton@gmail.com>
2020-02-20 23:20:59 +0000

    added seperate modified wp dirs

--- shop/wp-content/themes/modern-store/sidebar-store.php
+++
@@ -1,6 +0,0 @@
-<?php
-if ( is_active_sidebar( 'store' ) ) : ?>
-    <aside class="sidebar sidebar-store" id="sidebar-store" role="complementary">
-        <?php dynamic_sidebar( 'store' ); ?>
-    </aside>
-<?php endif;
